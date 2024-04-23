8dc194a0cedc504f2c9b7d9cefe9160bae4f9ae8
Brandon Chau <bchau03@gmail.com>
2024-04-23 12:00:53 -0700

    fixed arraytests and arrayexamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,18 +1,22 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = { 3, 2 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{ 2, 3 }, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = { 2, 3, 7, 2};
+    assertArrayEquals(new int[]{ 2, 7, 3, 2}, ArrayExamples.reversed(input1));
   }
+
 }
